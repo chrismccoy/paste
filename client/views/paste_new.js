764dac0a94c9ddc00c2453df5cd738dc95e7c2a2
chrismccoy <fris@fris.net>
2025-01-17 18:26:06 +0000

    only use shades of purple for paste view

--- client/views/paste_new.js
+++ client/views/paste_new.js
@@ -2,15 +2,14 @@ import { getCookie, setCookie } from "../cookies.js";
 
 const pasteNew = () => {
     const editor = document.getElementById("editor");
-    const languageSelector = document.getElementById("language");
-    
     editor.focus();
 
     const lang = getCookie("language") || "plaintext";
-    languageSelector.value = lang;
+    const pickLanguage = document.getElementById("language");
+    pickLanguage.value = lang;
 
-    languageSelector.addEventListener("change", (event) => {
-        setCookie("language", event.target.value, { expires: Infinity });
+    pickLanguage.addEventListener("change", ({ target }) => {
+        setCookie("language", target.value, { until: Infinity });
     });
 };
 
--- client/views/paste_show.js
+++ client/views/paste_show.js
@@ -2,13 +2,13 @@ import { THEMES } from "../themes.js";
 import { LANGUAGES } from "../languages.js";
 import { getCookie, setCookie } from "../cookies.js";
 
-const themeChosen = getCookie("theme") || THEMES.default;
+const themeChosen = THEMES.default;
 
 function renderLineNumbers() {
     const blocks = document.querySelectorAll('pre code.hljs');
     const lineNumbers = blocks[0];
-    const rawCode = blocks[1].innerHTML;
-    const lines = rawCode.split("\n");
+    const codeBlock = blocks[1];
+    const lines = codeBlock.innerHTML.split("\n");
     lineNumbers.innerHTML = lines.map((_, index) => index + 1).join("\n");
 }
 
@@ -19,31 +19,15 @@ function fixupUI(theme) {
 }
 
 const pasteShow = () => {
-    const themesEl = document.getElementById("themes");
-    themesEl.innerHTML = ""; // Clear existing options
-
-    THEMES.forEach(theme => {
-        const option = document.createElement("option");
-        option.value = theme.file;
-        option.innerHTML = theme.name;
-        option.selected = themeChosen === theme.file;
-        themesEl.appendChild(option);
-    });
-
-    fixupUI(themesEl.value);
+    const theme = 'shades-of-purple.css';
+    fixupUI(theme);
     renderLineNumbers();
 
-    const codeBlock = document.querySelectorAll('pre code.hljs')[1];
-    hljs.highlightBlock(codeBlock);
-
-    themesEl.addEventListener("change", (event) => {
-        const themeLink = document.getElementById("hljsTheme");
-        if (event.target.value) {
-            themeLink.href = `/css/v/${event.target.value}`;
-            fixupUI(event.target.value);
-            setCookie("theme", event.target.value, { expires: Infinity });
-        }
-    });
+    const code = document.querySelectorAll('pre code.hljs')[1];
+    hljs.highlightBlock(code);
+    // Uncomment the following lines if you want to display the language
+    // const lang = document.getElementById("language");
+    // lang.innerHTML = LANGUAGES[code.result.language] || code.result.language;
 };
 
 export { pasteShow };
