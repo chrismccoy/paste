8658d0295f9f3b77f310a9fffb3468d7657ee4d5
chrismccoy <fris@fris.net>
2025-01-17 17:22:47 +0000

    refactored rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,10 @@ const commonjs = require('rollup-plugin-commonjs');
 module.exports = {
     input: 'client/boot.js',
     output: {
-      file: 'public/js/bundle.js',
-      format: 'iife',
-      name: 'app',
+        file: 'public/js/bundle.js',
+        format: 'iife',
+        name: 'app',
     },
-    plugins: [
-      commonjs()
-    ],
-    treeshake: false
-  };
+    plugins: [commonjs()],
+    treeshake: false,
+};
