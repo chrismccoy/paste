06330339e5639104aec95f1bd886e016c75e0e07
chrismccoy <fris@fris.net>
2025-01-18 04:51:55 +0000

    moved test build script to main build script

--- build.js
+++ build.js
@@ -1,18 +1,30 @@
-const exec = require('child_process').execSync
+const { execSync } = require('child_process');
+const { LANGUAGES, skipBuild } = require("./client/languages");
 
-function run(cmd, opts = {}) {
-    opts.stdio = ["inherit", "inherit", "inherit"]
-    exec(cmd, opts)
+function runCommand(cmd, opts = {}) {
+    opts.stdio = ["inherit", "inherit", "inherit"];
+    try {
+        execSync(cmd, opts);
+    } catch (error) {
+        console.error(`Error executing command: ${cmd}\n`, error.message);
+        process.exit(1);
+    }
 }
 
-const { LANGUAGES, skipBuild } = require("./client/languages")
-const languages = Object.keys(LANGUAGES).filter((l) => !skipBuild.includes(l))
+const languages = Object.keys(LANGUAGES).filter((l) => !skipBuild.includes(l));
 
-run("node ./db.js");
-run(`node ./tools/build.js -t browser ${languages.join(" ")}`, {cwd: "./work/highlight.js"})
-run(`cp work/highlight.js/build/highlight.min.js public/js/highlight.js`)
-run("./node_modules/.bin/rollup -c")
-run("gzip -f -k public/js/highlight.js")
-run("brotli -f -k public/js/highlight.js")
-run("gzip -f -k public/js/bundle.js")
-run("brotli -f -k public/js/bundle.js")
+const commands = [
+    "node ./db.js",
+    `node ./tools/build.js -t browser ${languages.join(" ")}`,
+    "cp work/highlight.js/build/highlight.min.js public/js/highlight.js",
+    "./node_modules/.bin/rollup -c",
+    "gzip -f -k public/js/highlight.js",
+    "brotli -f -k public/js/highlight.js",
+    "gzip -f -k public/js/bundle.js",
+    "brotli -f -k public/js/bundle.js"
+];
+
+commands.forEach((command, index) => {
+    const options = index === 1 ? { cwd: "./work/highlight.js" } : {};
+    runCommand(command, options);
+});
