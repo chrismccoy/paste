3fa665d4851ee62de9f3d5456205b071aa3d196c
chrismccoy <fris@fris.net>
2025-01-19 04:52:57 +0000

    added a check if table exists or not in sqlite db

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+const fs = require("fs").promises;
+
 const knex = require("knex")({
   client: "sqlite3",
   useNullAsDefault: true,
@@ -8,29 +10,23 @@ const knex = require("knex")({
 
 const createPastesTable = async () => {
   try {
-    await knex.schema.createTable("pastes", (table) => {
-      table.increments("id");
-      table.string("language");
-      table.string("uuid", 100);
-      table.string("key", 100).unique();
-      table.string("content", 100000).notNullable();
-      table.datetime("expires_at").notNullable();
-      table.timestamps(false, true);
-    });
-    console.log("Pastes database created successfully.");
+    const exists = await knex.schema.hasTable("pastes");
+    if (!exists) {
+      await knex.schema.createTable("pastes", (table) => {
+        table.increments("id");
+        table.string("language");
+        table.string("uuid", 100);
+        table.string("key", 100).unique();
+        table.string("content", 100000).notNullable();
+        table.datetime("expires_at").notNullable();
+        table.timestamps(false, true);
+      });
+      console.log("Pastes table created successfully.");
+    } else {
+      console.log("Pastes table already exists.");
+    }
   } catch (error) {
-    console.error("Error creating pastes database:", error);
-  } finally {
-    await knex.destroy();
-  }
-};
-
-const dropPastesTable = async () => {
-  try {
-    await knex.schema.dropTableIfExists('pastes');
-    console.log("Pastes database dropped.");
-  } catch (error) {
-    console.error("Error dropping pastes database:", error);
+    console.error("Error creating pastes table:", error);
   } finally {
     await knex.destroy();
   }
@@ -40,8 +36,10 @@ const checkDatabaseExists = async () => {
   try {
     await fs.access('./db/db.sqlite');
     console.log('Paste Database exists.');
+    await createPastesTable();
   } catch (error) {
-      createPastesTable()
+    console.log('Paste Database does not exist, creating it now.');
+    await createPastesTable();
   }
 };
 
