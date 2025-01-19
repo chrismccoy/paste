2409bbbf3dd044264c5672c260f4f50b7652b299
chrismccoy <fris@fris.net>
2025-01-19 02:21:58 +0000

    removed dracula theme

--- client/themes.js
+++ client/themes.js
@@ -1,11 +1,9 @@
 const POPULAR = [
-    { name: "Dark", file: "dracula.css", dark: true },
     { name: "Nord", file: "nord.css", dark: true },
     { name: "Shades of Purple", file: "shades-of-purple.css", dark: true },
 ]
 
 const THEMES = [
-    {name: "dracula.css", file: "dracula.css" },
     {name: "nord.css", file: "nord.css" },
     {name: "shades-of-purple.css", file: "shades-of-purple.css" }
 ]
