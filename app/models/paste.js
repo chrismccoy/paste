cf47cacbad2100cbd392ff82eaef9ffebc992019
chrismccoy <fris@fris.net>
2025-01-22 17:19:59 +0000

    changed library for generating the slugs

--- app/models/paste.js
+++ app/models/paste.js
@@ -1,5 +1,5 @@
-const { Model } = require('objection');
-const gfynonce = require('gfynonce');
+const { Model } = require("objection");
+const slug = require("ultra-ignorant-aardvark");
 const { LANGUAGES } = require("../../client/languages");
 
 const toUTC = (date) => date; // Simplified function
@@ -13,93 +13,100 @@ const DAY_IN_MILLIS = HOURS_PER_DAY * MINUTES_IN_HOUR * SECONDS_PER_MIN * 1000;
 const YEAR_IN_MILLIS = DAY_IN_MILLIS * 365;
 
 class Paste extends Model {
-  constructor(data) {
-    super();
-    if (data) {
-      Object.assign(this, data);
+    constructor(data) {
+        super();
+        if (data) {
+            Object.assign(this, data);
+        }
     }
-  }
 
-  $beforeInsert() {
-    this.expires_at = new Date(Date.now() + YEAR_IN_MILLIS).toISOString();
-    this.uuid = gfynonce({ adjectives: 4, separator: '-' });
-    this.key = this.uuid;
-  }
-
-  $afterGet() {
-    this.expires_at = new Date(this.expires_at);
-  }
-
-  get ttl() {
-    return (this.expires_at - toUTC(new Date())) / 1000; // Return in seconds
-  }
-
-  get ttlText() {
-    const ttlInSeconds = this.ttl;
-    const ttlInHours = ttlInSeconds / 3600;
-    const ttlInDays = ttlInSeconds / (DAY_IN_MILLIS / 1000);
-    const ttlInMonths = ttlInSeconds / (YEAR_IN_MILLIS / 1000);
-
-    if (ttlInSeconds < 60) {
-      return `${round(ttlInSeconds, 0)} seconds`;
-    } else if (ttlInSeconds < 3600) {
-      return `${round(ttlInSeconds / SECONDS_PER_MIN, 0)} minutes`;
-    } else if (ttlInHours < 24) {
-      return `${round(ttlInHours, 0)} hours`;
-    } else if (ttlInDays < 365) {
-      return `${round(ttlInDays, 0)} days`;
-    } else {
-      return `${round(ttlInMonths, 0)} months`;
+    $beforeInsert() {
+        this.expires_at = new Date(Date.now() + YEAR_IN_MILLIS).toISOString();
+        this.uuid = slug({
+            includeRandomAdverbs: true,
+            includePredicate: true,
+            delimiter: ".",
+            capitalizeFirstWord: false,
+            capitalizeAllWords: false,
+            includeRandomEndingPunctuation: false,
+        });
+        this.key = this.uuid;
     }
-  }
 
-  get kbSize() {
-    return this.content.length / 1000;
-  }
+    $afterGet() {
+        this.expires_at = new Date(this.expires_at);
+    }
 
-  get kbSizeText() {
-    return this.kbSize > 1 ? `${round(this.kbSize, 1)}<sup>KB</sup>` : "";
-  }
+    get ttl() {
+        return (this.expires_at - toUTC(new Date())) / 1000; // Return in seconds
+    }
 
-  get languageName() {
-    return LANGUAGES[this.language] || this.language;
-  }
+    get ttlText() {
+        const ttlInSeconds = this.ttl;
+        const ttlInHours = ttlInSeconds / 3600;
+        const ttlInDays = ttlInSeconds / (DAY_IN_MILLIS / 1000);
+        const ttlInMonths = ttlInSeconds / (YEAR_IN_MILLIS / 1000);
 
-  get isExpired() {
-    return this.ttl < 0;
-  }
+        if (ttlInSeconds < 60) {
+            return `${round(ttlInSeconds, 0)} seconds`;
+        } else if (ttlInSeconds < 3600) {
+            return `${round(ttlInSeconds / SECONDS_PER_MIN, 0)} minutes`;
+        } else if (ttlInHours < 24) {
+            return `${round(ttlInHours, 0)} hours`;
+        } else if (ttlInDays < 365) {
+            return `${round(ttlInDays, 0)} days`;
+        } else {
+            return `${round(ttlInMonths, 0)} months`;
+        }
+    }
 
-  get lineCount() {
-    return this.content.split("\n").length;
-  }
+    get kbSize() {
+        return this.content.length / 1000;
+    }
 
-  static async deleteExpired() {
-    const ONE_YEAR_AGO = new Date(Date.now() - YEAR_IN_MILLIS);
-    return await Paste.query().delete().where("expires_at", "<", ONE_YEAR_AGO);
-  }
+    get kbSizeText() {
+        return this.kbSize > 1 ? `${round(this.kbSize, 1)}<sup>KB</sup>` : "";
+    }
 
-  static async expired() {
-    const ONE_YEAR_AGO = new Date(Date.now() - YEAR_IN_MILLIS);
-    return await Paste.query().where("expires_at", "<", ONE_YEAR_AGO);
-  }
+    get languageName() {
+        return LANGUAGES[this.language] || this.language;
+    }
 
-  static async findByKey(key) {
-    return await Paste.query().where("key", "=", key).first();
-  }
+    get isExpired() {
+        return this.ttl < 0;
+    }
 
-  async destroy() {
-    await Paste.query().deleteById(this.id);
-  }
+    get lineCount() {
+        return this.content.split("\n").length;
+    }
 
-  async save() {
-    await Paste.query().insert(this);
-    const data = await Paste.query().findById(this.id);
-    Object.assign(this, data);
-  }
+    static async deleteExpired() {
+        const ONE_YEAR_AGO = new Date(Date.now() - YEAR_IN_MILLIS);
+        return await Paste.query().delete().where("expires_at", "<", ONE_YEAR_AGO);
+    }
 
-  static get tableName() {
-    return 'pastes';
-  }
+    static async expired() {
+        const ONE_YEAR_AGO = new Date(Date.now() - YEAR_IN_MILLIS);
+        return await Paste.query().where("expires_at", "<", ONE_YEAR_AGO);
+    }
+
+    static async findByKey(key) {
+        return await Paste.query().where("key", "=", key).first();
+    }
+
+    async destroy() {
+        await Paste.query().deleteById(this.id);
+    }
+
+    async save() {
+        await Paste.query().insert(this);
+        const data = await Paste.query().findById(this.id);
+        Object.assign(this, data);
+    }
+
+    static get tableName() {
+        return "pastes";
+    }
 }
 
 module.exports = Paste;
